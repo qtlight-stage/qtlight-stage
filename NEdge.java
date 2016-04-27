57c79e989e5986abb47a17f96e52da2c9517d7e6
anthi7 <anthi7@naver.com>
2016-04-27 21:54:02 +0900

    Update NEdge.java

--- NEdge.java
+++ NEdge.java
@@ -2,7 +2,7 @@ public class NEdge {
 	private NVertex Nstart;
 	private NVertex Nend;
 	
-	public void create(NVertex start, NVertex end){
+	public void init(NVertex start, NVertex end){
 		this.Nstart = start;
 		this.Nend = end;
 	}
