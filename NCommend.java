d017e3ec036f3cad70d582a817833ae302d326a9
anthi7 <anthi7@naver.com>
2016-05-06 00:25:35 +0900

    server로 commend를 보내는 class가 될 예정

---
+++ NCommend.java
@@ -0,0 +1,24 @@
+package NMindMap;
+
+import NMindMap.NFrame.Node;
+
+public class NCommend {
+	public void commendAddNode(NFrame f, String contents, int x, int y, int width, int height){
+		f.addNode(contents, x, y, width, height);
+	}
+	public void commendRemoveNode(NFrame f, Node n){
+		f.removeNode(n);
+	}
+	public void commendAddArrow(NFrame f, Node start, Node end){
+		f.addArrow(start, end);
+	}
+	public void commendRemoveArrow(NFrame f, Node start, Node end){
+		f.removeArrow(start, end);
+	}
+	public void commendEditNode(NFrame f, Node n, String contents, int width, int height){
+		f.editNode(n, contents, width, height);
+	}
+	public void commendMoveNode(NFrame f, Node n, int x, int y){
+		f.moveNode(n, x, y);
+	}
+}
