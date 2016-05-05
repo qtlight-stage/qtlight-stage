147655c66e66d7be5783df6e6443d1dc56836e65
anthi7 <anthi7@naver.com>
2016-05-06 00:26:14 +0900

    네트워크 모듈을 위한 테스트 코드 및 NCommend 적용

--- NMain.java
+++ NMain.java
@@ -2,13 +2,32 @@ package NMindMap;
 import javax.swing.JFrame;
 
 public class NMain {
+	static int frameWidth = 1000;
+	static int frameHeight = 500;
+	static NFrame mainFrame=new NFrame(frameWidth, frameHeight);
 	public static void main(String[] args) 
 	{
-		int frameWidth = 1000;
-		int frameHeight = 500;
-		NFrame s=new NFrame(frameWidth, frameHeight);
-		s.setSize(frameWidth, frameHeight);
-		s.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		s.setVisible(true);
+		NCommend C = new NCommend();
+		mainFrame.setMain(C);
+		mainFrame.setSize(frameWidth, frameHeight);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setVisible(true);
+		
+		//아래부분에 네트워크 통신 부분 필요
+		long start = System.currentTimeMillis();
+		long end = System.currentTimeMillis();
+		int x = 100;
+		while(true){
+			end = System.currentTimeMillis();
+			if (end-start>5000){
+				C.commendAddNode(mainFrame, "test", x, x, 120, 25);
+				System.out.println("hello");
+				start = end;
+				x = x + 20;
+			}
+				
+		}
 	}
+	
+	
 }
