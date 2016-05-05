e11bb25362d2ce5e573757b7fa34394872f68928
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-05-05 23:12:51 +0900

    move package declaration to top

--- src/main/java/NData.java
+++ src/main/java/NData.java
@@ -1,7 +1,7 @@
+package NMindMap;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
-package NMindMap;
 import java.util.List;
 import java.util.LinkedList;
 
