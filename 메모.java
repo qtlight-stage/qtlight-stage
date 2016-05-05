b75175e9c1c24ed0ea1bfe4011925b6e481a640e
anthi7 <anthi7@naver.com>
2016-05-06 00:28:28 +0900

    필요없는 메모 삭제

--- 메모.java
+++ 메모.java
@@ -1,28 +1,6 @@
 //모든 메모는 여기에 위키 불편함
 //메모에 대한 구상이 구체화되면 코드로 하던가 위키로 보내던가하셈
 
-//JFrame에서
-//Node 그릴때 예시
-
-public class frameName extends JFrame {
-    public void makeNewNode(String Vcontents, int Vx, int Vy, int Vwidth, int Vheight){
-      JTextPane newNode = new JTextPane();
-      newNode.setText(Vcontents);
-      newNode.setBounds(Vx, Vy, Vwidth, Vheight);
-      getContentPane().add(NewNode);
-}
-//JTextPane이 id도 저장할수있게 child class 정의 해야함
-//이때 예시
-public class frameName2 extends JFrame {
-    public void makeNewNode(int Vid, String Vcontents, int Vx, int Vy, int Vwidth, int Vheight){
-      NNode newNode = new JTextPane();
-      newNode.setId(Vid);
-      newNode.setText(Vcontents);
-      newNode.setBounds(Vx, Vy, Vwidth, Vheight);
-      getContentPane().add(NewNode);
-}
-//NNode extends JTextPane
-
 //예외처리 예시
 		try {
 			throw new Exception();
