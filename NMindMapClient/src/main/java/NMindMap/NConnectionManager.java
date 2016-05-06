bdf5824d1eb580f7115cafe727223963284a0341
Kagami Sascha Rosylight <saschanaz@outlook.com>
2016-05-06 20:42:27 +0900

    use Future

--- NMindMapClient/src/main/java/NMindMap/NConnectionManager.java
+++ NMindMapClient/src/main/java/NMindMap/NConnectionManager.java
@@ -1,14 +1,12 @@
 package NMindMap;
 
-import com.sun.xml.internal.ws.util.CompletedFuture;
-
+import javax.json.JsonObject;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
-import java.nio.channels.CompletionHandler;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 /**
@@ -17,76 +15,61 @@ import java.util.concurrent.Future;
 public class NConnectionManager {
     private static AsynchronousSocketChannel ch;
 
-    public static Future<Void> connectToServer() throws IOException {
-        CompletableFuture<Void> future = new CompletableFuture<>();
-
+    public static void connectToServer() throws IOException, ExecutionException, InterruptedException {
         ch = AsynchronousSocketChannel.open();
 
-        ch.connect(new InetSocketAddress("localhost", 8080), null, new CompletionHandler<Void, Void>() {
-            @Override
-            public void completed(Void result, Void attachment) {
-                System.out.print("Connected to server\n");
+        Future<Void> future = ch.connect(new InetSocketAddress("localhost", 8080));
+        future.get();
 
-                handleChannel(ch);
-                future.complete(null);
-            }
-
-            @Override
-            public void failed(Throwable exc, Void attachment) {
-                future.completeExceptionally(exc);
-            }
-        });
-
-        return future;
+        System.out.print("Connected to server\n");
+        handleChannel(ch);
     }
 
-    private static void handleChannel(final AsynchronousSocketChannel ch) {
+    private static void handleChannel(final AsynchronousSocketChannel ch) throws ExecutionException, InterruptedException, IOException {
         final ByteBuffer buffer = ByteBuffer.allocate(1024);
         final StringBuilder stringBuilder = new StringBuilder();
 
-        readContinuously(ch, buffer, stringBuilder);
+        while (ch.isOpen()) {
+            read(ch, buffer, stringBuilder);
+        }
     }
 
-    private static void readContinuously(AsynchronousSocketChannel ch, ByteBuffer buffer, StringBuilder stringBuilder) {
-        ch.read(buffer, buffer, new CompletionHandler<Integer, ByteBuffer>() {
-            public void completed(Integer bytesRead, ByteBuffer buffer) {
-                if (bytesRead == 0) {
-                    if (ch.isOpen()) {
-                        readContinuously(ch, buffer, stringBuilder);
-                    }
-                    return;
-                }
+    private static void read(AsynchronousSocketChannel ch, ByteBuffer buffer, StringBuilder stringBuilder) throws ExecutionException, InterruptedException, IOException {
+        Future<Integer> future = ch.read(buffer);
 
-                buffer.flip();
-                final String str = StandardCharsets.UTF_8.decode(buffer).toString();
-                buffer.flip();
-                System.out.print("Received partially: " + str + "\n");
-                final int newlineIndex = str.indexOf('\n');
+        int bytesRead = future.get();
 
-                if (newlineIndex == -1) {
-                    stringBuilder.append(str);
-                } else {
-                    stringBuilder.append(str.substring(0, newlineIndex));
-
-                    // new complete message has arrived, handle this!
-                    final String built = stringBuilder.toString();
-                    System.out.print("Received message: " + built + "\n");
-
-                    stringBuilder.setLength(0);
-                }
-
-                if (ch.isOpen()) {
-                    readContinuously(ch, buffer, stringBuilder);
-                }
+        if (bytesRead == 0) {
+            if (ch.isOpen()) {
+                read(ch, buffer, stringBuilder);
             }
+            return;
+        }
 
-            public void failed(Throwable exc, ByteBuffer attachment) {
-                try {
-                    ch.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        buffer.flip();
+        final String str = StandardCharsets.UTF_8.decode(buffer).toString();
+        buffer.flip();
+        System.out.print("Received partially: " + str + "\n");
+        final int newlineIndex = str.indexOf('\n');
+
+        if (newlineIndex == -1) {
+            stringBuilder.append(str);
+        } else {
+            stringBuilder.append(str.substring(0, newlineIndex));
+
+            // new complete message has arrived, handle this!
+            final String built = stringBuilder.toString();
+            System.out.print("Received message: " + built + "\n");
+
+            stringBuilder.setLength(0);
+        }
+
+        if (ch.isOpen()) {
+            read(ch, buffer, stringBuilder);
+        }
+    }
+
+    public Future<Integer> sendJson(JsonObject json) {
+        return ch.write(StandardCharsets.UTF_8.encode(json.toString()));
     }
 }
